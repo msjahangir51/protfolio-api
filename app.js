a742729b9862528113e9f8c2f892171ff581a13e
msjahangir51 <ms.jahangir51@gmail.com>
2024-05-24 12:53:03 +0600

    cors allow problem fix

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const createError = require("http-errors")
 const multer = require("multer");
 const { projectModel } = require("./models/projects.model");
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+const cors = require('cors');
+const corsOptions ={
+    origin: "*", 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
